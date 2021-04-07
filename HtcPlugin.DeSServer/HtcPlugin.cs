abe7fac30d5e890e1394d1ae112185ab6b95562d
João Pedro Caires <jpdante@tryhosting.com.br>
2021-04-07 03:52:42 -0300

    Add endpoint filter

--- HtcPlugin.DeSServer/HtcPlugin.cs
+++ HtcPlugin.DeSServer/HtcPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using HtcPlugin.DeSServer.Core;
@@ -21,6 +22,7 @@ namespace HtcPlugin.DeSServer {
         internal static ILogger Logger { get; private set; }
         internal static DeSConfig Config { get; private set; }
         internal static Core.DeSServer Server { get; private set; }
+        internal static string Endpoint { get; private set; }
 
         public async Task Load(PluginServerContext pluginServerContext, ILogger logger) {
             Logger = logger;
@@ -31,6 +33,7 @@ namespace HtcPlugin.DeSServer {
             }
             Config = await ConfigManager.LoadFromFileAsync<DeSConfig>(configPath);
 
+            Endpoint = $"0.0.0.0:{Config.DeSServer.Port}";
             _ = new DatabaseContext(Config.Db);
             Server = new Core.DeSServer();
 
@@ -47,6 +50,10 @@ namespace HtcPlugin.DeSServer {
 
         public bool IsCompatible(int htcMajor, int htcMinor, int htcPatch) {
             return true;
+        }
+
+        public override Task<bool> BeforePageRequest(HttpContext httpContext, string filename) {
+            return Task.FromResult(!httpContext.ServerInfo.Endpoints.Contains(Endpoint));
         }
 
         public override async Task ThrowException(HttpContext httpContext, Exception exception) {
